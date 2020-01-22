f340fc741228d7fa3939032ae6f0951ff49d0366
Dr. Yad Tahir <yad@ieee.org>
2020-01-22 15:35:36 +0300

    Update portage package requirements

--- src/packages-portage.sh
+++ src/packages-portage.sh
@@ -2,4 +2,7 @@
 
 echo 'Run emerge to install required packages...'
 
-sudo emerge --ask --verbose dev-ruby/sass media-gfx/inkscape media-gfx/optipng
+sudo emerge --ask --verbose dev-ruby/sass \
+	 media-gfx/inkscape \
+	 media-gfx/optipng \
+	 x11-themes/gtk-engines-murrine
